a2f641a0900dea9e2827506683c1bdab3a5e6cad
SanjitMaharjan <sanjitmhrjn@gmail.com>
2023-08-09 08:33:32 +0545

    admin user add and delete completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 
@@ -60,5 +61,36 @@ class UserController extends Controller
         auth()->logout();
 
         return redirect('/')->with('success', 'You have been logout');
+    }
+
+    public function addAdminUserPage()
+    {
+        $users = User::where("is_admin", true)->get();
+        foreach ($users as $user) {
+            $user->isCurrent = $user->id === Auth::user()->id;
+        }
+        return view("admin_user", compact('users'));
+    }
+
+    public function addAdminUser()
+    {
+        $attributes = request()->validate([
+            'name' => "required|max:255",
+            'phone_number' => "required|min:10|max:10",
+            'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email')],
+            'password' => 'required|min:7|max:255',
+        ]);
+        $attributes['password'] = bcrypt($attributes['password']);
+        $attributes['is_admin'] = true;
+        $user = User::create($attributes);
+        session()->flash('success', 'Admin has been created');
+        return redirect('/admin/users');
+    }
+
+    public function removeAdminUser(int $id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect("/admin/users");
     }
 }
